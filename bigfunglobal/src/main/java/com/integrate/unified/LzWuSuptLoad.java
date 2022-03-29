b7268006fa7a5b6a118320559577d2782d9f3140
yanwenqiang <dream@hzyouliang.com>
2022-03-29 21:32:24 +0800

    V1.2.8 依赖冲突

--- bigfunglobal/src/main/java/com/integrate/unified/LzWuSuptLoad.java
+++ bigfunglobal/src/main/java/com/integrate/unified/LzWuSuptLoad.java
@@ -34,15 +34,11 @@ import com.integrate.unified.gwdlhmkkm.GoogleQueryPayListener;
 import com.integrate.unified.gwdlhmkkm.GoogleQueryPurchaseListener;
 import com.integrate.unified.gwdlhmkkm.MyBillingImpl;
 import com.integrate.unified.lveterksl.BFAdjustListener;
-import com.integrate.unified.lveterksl.LoginModel;
 import com.integrate.unified.mphqzrzwd.BigFunViewModel;
 import com.integrate.unified.mphqzrzwd.SdkConfigurationInfoBean;
 import com.integrate.unified.tyiyfvohom.AdBFPlatForm;
 import com.integrate.unified.tyiyfvohom.AdBFSize;
 
-
-import com.google.android.gms.auth.api.identity.GetSignInIntentRequest;
-import com.google.android.gms.auth.api.identity.SignInClient;
 
 import com.google.gson.Gson;
 import com.integrate.unified.utiuqjyrti.EmulatorDetector;
@@ -74,7 +70,7 @@ public class LzWuSuptLoad {
     private static String TDid="";
 
     //    private MyBillingImpl myBilling;
-    private static GetSignInIntentRequest mGetSignInIntentRequest;
+//    private static GetSignInIntentRequest mGetSignInIntentRequest;
     private static JSONObject fbgv = new JSONObject();
 
     //获取时间
@@ -113,7 +109,7 @@ public class LzWuSuptLoad {
         mChannelCode = channelCode;
         SourceNetWork.initListener();
 //        TMNetWork.init();
-        LoginModel.getInstance();
+//        LoginModel.getInstance();
         MyBillingImpl.getInstance().initialize(mContext);
         ExceptionHandler.install(new ExceptionHandler.CustomExceptionHandler() {
             @Override
@@ -513,38 +509,38 @@ public class LzWuSuptLoad {
     }
 
     private static void Googleinit(String clientId) {
-        mGetSignInIntentRequest =
-                GetSignInIntentRequest.builder()
-                        .setServerClientId(clientId)
-                        .build();
+//        mGetSignInIntentRequest =
+//                GetSignInIntentRequest.builder()
+//                        .setServerClientId(clientId)
+//                        .build();
     }
 
     /**
-     * @param activity Activity上下文
+//     * @param activity Activity上下文
      */
 
 
-    @Keep
-    public static void BigFunLogin(Activity activity) {
+//    @Keep
+//    public static void BigFunLogin(Activity activity) {
+//
+//        if (checkSdkNotInit()) {
+//            return;
+//        }
+//        if (!BigFunViewModel.google || mGetSignInIntentRequest == null) {
+//            Log.e("BigFunSDK", "Background not set");
+//            return;
+//        }
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("BFLogin_Google", "Google");
+//        onEvent(mContext, "BFLogin_Google", map);
+//        LoginModel.Login(activity, mGetSignInIntentRequest);
+//    }
 
-        if (checkSdkNotInit()) {
-            return;
-        }
-        if (!BigFunViewModel.google || mGetSignInIntentRequest == null) {
-            Log.e("BigFunSDK", "Background not set");
-            return;
-        }
-        Map<String, Object> map = new HashMap<>();
-        map.put("BFLogin_Google", "Google");
-        onEvent(mContext, "BFLogin_Google", map);
-        LoginModel.Login(activity, mGetSignInIntentRequest);
-    }
 
-
-    @Keep
-    public static SignInClient BigFunIdentity() {
-        return LoginModel.BigFunIdentity();
-    }
+//    @Keep
+//    public static SignInClient BigFunIdentity() {
+//        return LoginModel.BigFunIdentity();
+//    }
 
 
 //    /**
@@ -570,10 +566,10 @@ public class LzWuSuptLoad {
 //        onEvent(mContext, "BFLogin_FB", map);
 //        LoginModel.facebookLogin(context, permissionList, listener);
 //    }
-    @Keep
-    public static void BigFunLogout() {
-        LoginModel.BigFunLogout();
-    }
+//    @Keep
+//    public static void BigFunLogout() {
+//        LoginModel.BigFunLogout();
+//    }
 
     /**
      *内购商品的展示
